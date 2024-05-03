6b6c60001ecc6a33e03079f98871f10a3edd71c3
ElieOko <elieoko100@gmail.com>
2024-05-03 15:05:51 +0100

    feat : add new color

--- app/src/main/java/com/example/jc_component/ui/theme/Color.kt
+++ app/src/main/java/com/example/jc_component/ui/theme/Color.kt
@@ -3,9 +3,20 @@ package com.example.jc_component.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
+val Vacant = Color(0xFFF0F8FF)
+val WhiteOff = Color(0xFFF0F8FF)
+val AliceBlue = Color(0xFFFAF9F6)
+val GostWhite = Color(0xFFF8F8FF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Blue300 = Color(0xFF1477C4)
+val Blue350 = Color(0xF2145CEB)
+val Green100 = Color(0xF029D53B)
+
+val Black100 = Color(0xF009161D)
+val Grey100 = Color(0xC63D3C42)
