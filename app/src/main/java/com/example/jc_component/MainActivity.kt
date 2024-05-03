cada3de3c7310e247276beb0448e52b21e1e0ec8
ElieOko <elieoko100@gmail.com>
2024-05-03 15:08:07 +0100

    fix : other retouche

--- app/src/main/java/com/example/jc_component/MainActivity.kt
+++ app/src/main/java/com/example/jc_component/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.jc_component.component.button.MButton
 import com.example.jc_component.ui.theme.Jc_componentTheme
+import com.example.jc_component.ui.theme.WhiteOff
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +23,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = WhiteOff
                 ) {
                     Column(
                         modifier = Modifier.fillMaxSize()
